17a2a792d554e286fb4fe8b78bc6573a9b708095
tedbrogan <55825802+tedbrogan@users.noreply.github.com>
2026-02-18 10:47:58 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-console.log("script.js loaded");
-console.log("supabase:", window.supabase);
-console.log("bcrypt:", window.bcrypt);
-
-// 16.1 — Initialize Supabase
+ // 16.1 — Initialize Supabase
 const SUPABASE_URL = "https://qgvgaswagloibanqbfbc.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_BSZwz-29LbC2iAJHpTxpBA_MbJQmQS2";
 
